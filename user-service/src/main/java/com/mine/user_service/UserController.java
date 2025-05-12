834c9e18da7d514d0f23f91c16f32ea94ac8eab1
Mine Safak <haticemine352@gmail.com>
2025-05-12 22:35:01 +0300

    eureka, user and order service
    demo

--- user-service/src/main/java/com/mine/user_service/UserController.java
+++ user-service/src/main/java/com/mine/user_service/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
     @GetMapping("/users")
     public String getUsers() {
         return "User list: Alice, Bob, Charlie";
